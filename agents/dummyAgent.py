00561aa0eddbea71ff8f925bec1c016629a062b3
yeriel <yeriel.paz@sansano.usm.cl>
2022-09-27 20:00:19 -0300

    base repo

---
+++ agents/dummyAgent.py
@@ -0,0 +1,25 @@
+import math, sys
+from lux.game import Game
+from lux.game_map import Cell, RESOURCE_TYPES
+from lux.constants import Constants
+from lux.game_constants import GAME_CONSTANTS
+from lux import annotate
+
+DIRECTIONS = Constants.DIRECTIONS
+game_state = None
+
+def agent(observation, configuration):
+    global game_state
+
+    ### Do not edit ###
+    if observation["step"] == 0:
+        game_state = Game()
+        game_state._initialize(observation["updates"])
+        game_state._update(observation["updates"][2:])
+        game_state.id = observation.player
+    else:
+        game_state._update(observation["updates"])
+    
+    actions = []
+    
+    return actions
